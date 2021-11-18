7dff47d86e0df866e4883f3bafa3f004417e8f9d
AbubakirovRA <AbubakirovRA@ya.ru>
2021-11-18 16:58:41 +0500

    Add task1 solution

--- workshop3/Program.cs
+++ workshop3/Program.cs
@@ -1,12 +1,5 @@
-﻿// 0. Вывести квадрат числа
-double squareNumber(double arg)
-{
-    return (arg * arg);
-}
-Console.WriteLine($"Квадрат введенного числа равен {squareNumber(UserDouble())}");
-
-
-double UserDouble()  //Метод ожидающий ввода вещественного числа и осуществляющий проверку ввода
+﻿//Метод ожидающий ввода вещественного числа и осуществляющий проверку ввода
+double UserDouble()
 {
     double Number = 0; //переменная Number, хранит вещественное число, которе вводит пользователь
     while (true)
@@ -25,3 +18,25 @@ double UserDouble()  //Метод ожидающий ввода веществе
     }
     return Number;        // возвращаем значение метода (функции) в программу
 }
+
+// 1. По двум заданным числам проверять является ли первое квадратом второго
+double squareNumber(double arg)
+{
+    return (arg * arg);
+}
+
+bool Test(double arg1, double arg2)
+{
+    return arg1 == squareNumber(arg2);// возвращается логическое значение true или false
+}
+
+Console.WriteLine("Введите первое число");
+double arg1 = UserDouble();
+
+Console.WriteLine("Введите второе число");
+double arg2 = UserDouble();
+
+Console.WriteLine($"Введено {arg1} и {arg2}");
+
+if (Test(arg1, arg2)) Console.WriteLine("Первое число является квадратом второго");// Если в конструкции if-else используются логические условия,
+else Console.WriteLine("Первое число не является квадратом второго"); // то достаточно после if поставить условие, возвращающее true-значение, а после else значение false

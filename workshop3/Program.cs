c5ad21289fe3ff1f4919a4629dd38ad47f5f14ab
AbubakirovRA <AbubakirovRA@ya.ru>
2021-11-21 00:21:03 +0500

    branch task5 changed

--- workshop3/Program.cs
+++ workshop3/Program.cs
@@ -1,2 +1,11 @@
-﻿// 45. Показать числа Фибоначчи
+﻿// 5. Написать программу вычисления значения функции y=f(a)
+// double Sinus()
+// {
+//     Console.WriteLine("Введите значение а");
+//     string A = Console.ReadLine();
+//     double a = double.Parse(A);
+//     return Math.Sin(a);
+// }
+
+// Console.WriteLine(Sinus()); 
 

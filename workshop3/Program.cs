dcf5479665892a82946878d8985d2484a534cf82
AbubakirovRA <AbubakirovRA@ya.ru>
2021-11-22 11:56:09 +0500

    Add task46 solution

--- workshop3/Program.cs
+++ workshop3/Program.cs
@@ -1,2 +1,16 @@
 ﻿// 46. Написать программу масштабирования фигуры
+int n = 2;
+int k = 5;
+int i = 0;
+int j = 0;
+Console.Clear();
+for (i = 0; i < n * k; i++)
+{
+    for (j = 0; j < n * k; j++)
+    {
+        Console.SetCursorPosition(i, j);
+        Console.Write("0");
+    }
+
+}
 

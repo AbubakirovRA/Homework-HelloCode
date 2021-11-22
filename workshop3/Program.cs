b7c6a810c9d70cb44318c1e63433680a29bcac81
AbubakirovRA <AbubakirovRA@ya.ru>
2021-11-22 11:32:36 +0500

    Add task45 solution

--- workshop3/Program.cs
+++ workshop3/Program.cs
@@ -1,1 +1,11 @@
 ﻿// 45. Показать числа Фибоначчи
+int F(int n)
+{
+    if (n == 1 || n == 2) return 1;
+    else return F(n - 1) + F(n - 2);
+}
+
+for (int i = 1; i < 25; i++)
+{
+    Console.WriteLine(F(i));
+}

9bd83d3bf4e7b6c16d7fd725a8f50fb294bc65aa
AbubakirovRA <AbubakirovRA@ya.ru>
2021-11-22 19:47:09 +0500

    add task47 solution

--- workshop3/Program.cs
+++ workshop3/Program.cs
@@ -3,7 +3,7 @@ int [] CreateArray(int arg, int arg1, int arg2)
     {
         int [] array = new int [arg];
         for( int i = 0; i < array.Length; i++) array[i] = new Random().Next(arg1,arg2);
-        Console.WriteLine(String.Join(',', array(10, -10, 10)));
+        //Console.WriteLine(String.Join(',', array(10, -10, 10)));
         return array;
     }
 

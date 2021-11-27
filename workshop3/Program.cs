1b8cb15c2ae3b8fb705382a97d0fe682bdc2e846
AbubakirovRA <91825137+AbubakirovRA@users.noreply.github.com>
2021-11-27 17:35:15 +0500

    Add changes task30

--- workshop3/Program.cs
+++ workshop3/Program.cs
@@ -10,3 +10,8 @@
 // {
 //     if (Cubic(i)%2==0) Console.WriteLine($"Cubic {i} = {Cubic(i)}");
 // }
+
+int Cubic(int arg) {return arg*arg*arg;}
+bool Even(int arg) {return arg%2==0;}
+int n = 5;
+for (int i=1; i<n; i++) {if (Even(Cubic(i))) Console.WriteLine((Cubic(i)));}

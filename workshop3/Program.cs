5b41f5551ba9275caa4b5114fb58aee20a2c577c
AbubakirovRA <AbubakirovRA@ya.ru>
2021-12-03 14:00:44 +0500

    add task66  solution

--- workshop3/Program.cs
+++ workshop3/Program.cs
@@ -1,71 +1,11 @@
-﻿// 57. Написать программу упорядочивания по убыванию элементов каждой строки двумерного массива.
-int[,] Sorting(int[,] array)
-{
-    PrintArray(array);
-    System.Console.WriteLine();
-    int j, temp = 0;
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int n = 0; n < array.GetLength(1); n++)
-        {
-            for (j = 0; j < array.GetLength(1) - 1; j++)
-            {
-                if (array[i, j] > (array[i, j + 1]))
-                {
-                    temp = array[i, j];
-                    array[i, j] = array[i, j + 1];
-                    array[i, j + 1] = temp;
-                }
-            }
-            j += n;
-        }
-    }
-    return array;
-}
-Console.Clear();
-PrintArray(Sorting(FillArray(Array(6, 11))));
+﻿
 // 66. Показать натуральные числа от 1 до N, N задано
-// int Natural(int i, int n)
-// {
-//     if (n == 0) return 0;
-//     else {Console.Write($"{i} ");
-//     return Natural(i += 1, n - 1);}
-// }
-
-// Natural(1, 15);
-
-
-
-
-
-int[,] Array(int m, int n) // метод создания массива
+int Natural(int i, int n)
 {
-    int[,] Array = new int[m, n];
-    return Array;
+    if (n == 0) return 0;
+    else {Console.Write($"{i} ");
+    return Natural(i += 1, n - 1);}
 }
 
-int[,] FillArray(int[,] Arr) // метод заполения массива 
-{
+Natural(1, 15);
 
-    for (int i = 0; i < Arr.GetLength(0); i++)
-    {
-        for (int j = 0; j < Arr.GetLongLength(1); j++)
-        {
-            Arr[i, j] = (new Random().Next(0, 10));
-        }
-    }
-    return Arr;
-}
-
-void PrintArray(int[,] Arr) // метод вывода массива в консоль
-{
-
-    for (int i = 0; i < Arr.GetLength(0); i++)
-    {
-        for (int j = 0; j < Arr.GetLongLength(1); j++)
-        {
-            Console.Write($" {Arr[i, j]}");
-        }
-        Console.WriteLine();
-    }
-}

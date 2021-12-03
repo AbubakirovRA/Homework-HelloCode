320de93d61c5c2b8cf948ace77849a2730e7b9d4
AbubakirovRA <AbubakirovRA@ya.ru>
2021-12-03 12:49:16 +0500

    add task57 solution

--- workshop3/Program.cs
+++ workshop3/Program.cs
@@ -1,59 +1,59 @@
-﻿// Написать программу упорядочивания по убыванию элементов каждой строки двумерного массива.
-// int[,] Sorting(int[,] array)
-// {
-//     for (int i = 0; i < array.GetLength(0);i++)
-//         {
-//             for (int j=0; j < array.GetLength(1); j++)
-//                 {
-
-//                 }
-//         }
-//     return array;
-// }
+﻿// 57. Написать программу упорядочивания по убыванию элементов каждой строки двумерного массива.
+int[,] Sorting(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0);i++)
+        {
+            for (int j=0; j < array.GetLength(1); j++)
+                {
+                    
+                }
+        }
+    return array;
+}
 
 
 // 66. Показать натуральные числа от 1 до N, N задано
-int Natural(int n)
+// int Natural(int i, int n)
+// {
+//     if (n == 0) return 0;
+//     else {Console.Write($"{i} ");
+//     return Natural(i += 1, n - 1);}
+// }
+
+// Natural(1, 15);
+
+
+
+
+
+int[,] Array(int m, int n) // метод создания массива
 {
-    Console.WriteLine($"{n} ");
-    if (n == 0) return n;
-    else return Natural(n - 1);
+    int[,] Array = new int[m, n];
+    return Array;
 }
 
-Natural(10);
+int[,] FillArray(int[,] Arr) // метод заполения массива 
+{
 
+    for (int i = 0; i < Arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < Arr.GetLongLength(1); j++)
+        {
+            Arr[i, j] = (new Random().Next(0, 10));
+        }
+    }
+    return Arr;
+}
 
+void PrintArray(int[,] Arr) // метод вывода массива в консоль
+{
 
-
-
-// int[,] Array(int m, int n) // метод создания массива
-// {
-//     int[,] Array = new int[m, n];
-//     return Array;
-// }
-
-// int[,] FillArray(int[,] Arr) // метод заполения массива 
-// {
-
-//     for (int i = 0; i < Arr.GetLength(0); i++)
-//     {
-//         for (int j = 0; j < Arr.GetLongLength(1); j++)
-//         {
-//             Arr[i, j] = (new Random().Next(0, 10));
-//         }
-//     }
-//     return Arr;
-// }
-
-// void PrintArray(int[,] Arr) // метод вывода массива в консоль
-// {
-
-//     for (int i = 0; i < Arr.GetLength(0); i++)
-//     {
-//         for (int j = 0; j < Arr.GetLongLength(1); j++)
-//         {
-//             Console.Write($"*{Arr[i, j]}");
-//         }
-//         Console.WriteLine();
-//     }
-// }
+    for (int i = 0; i < Arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < Arr.GetLongLength(1); j++)
+        {
+            Console.Write($" {Arr[i, j]}");
+        }
+        Console.WriteLine();
+    }
+}

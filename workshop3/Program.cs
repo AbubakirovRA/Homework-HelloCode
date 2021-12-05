0911d4963d5d1d41aed2a9019b01fdf39d2d756e
AbubakirovRA <AbubakirovRA@ya.ru>
2021-12-05 13:23:12 +0500

    add task 49 solution

--- workshop3/Program.cs
+++ workshop3/Program.cs
@@ -1,14 +1,15 @@
-﻿// 48/ Показать двумерный массив размером m×n заполненный целыми числами
-int[,] Main(int m, int n)
+﻿// 49 Показать двумерный массив размером m×n заполненный вещественными числами
+double[,] Main(int m, int n)
 {
-    int[,] array = new int[m, n];
+    double[,] array = new double[m, n];
     for (int i = 0; i < m; i++)
     {
         for (int j = 0; j < n; j++)
         {
-            array[i,j]=new Random().Next(10, 100);
-            Console.Write($"{array[i,j]} ");
+            array[i,j]=Math.Round((new Random().NextDouble()*10), 2); // Math.Round(double value, int digits) - округление
+            Console.Write($"{array[i,j]} ");         //value - число, которое нужно округлить, digits - количество цифр в дробной части, которые нужно оставить.
         }
+    
         Console.WriteLine();
     }
     return array;

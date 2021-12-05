b33b7e5c1ae546fc28d590e60dbfde70af8a7f04
AbubakirovRA <AbubakirovRA@ya.ru>
2021-12-05 16:08:58 +0500

    add task52 solution

--- workshop3/Program.cs
+++ workshop3/Program.cs
@@ -1,4 +1,4 @@
-﻿// 51. Задать двумерный массив следующим правилом: A[m,n]=m+n
+﻿// 52. В двумерном массиве заменить элементы, у которых оба индекса чётные на их квадраты
 int[,] Main(int m, int n)
 {
     int[,] array = new int[m, n];
@@ -6,13 +6,19 @@ int[,] Main(int m, int n)
     {
         for (int j = 0; j < n; j++)
         {
-            array[i,j]=i + j; 
-            Console.Write($"{array[i,j]} ");
+            array[i, j] = new Random().Next(10, 100);
+            if (i % 2 == 0 && j % 2 == 0) array[i, j] = squareNumber(array[i, j]);
+            Console.Write($"{array[i, j]} ");
         }
-    
+
         Console.WriteLine();
     }
     return array;
 }
 
-Main(5,5);
+int squareNumber(int arg)
+{
+    return (arg * arg);
+}
+
+Main(5, 5);

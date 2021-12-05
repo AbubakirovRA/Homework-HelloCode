a71e59bccd829b11cbfc7f44a94411e229efdd94
AbubakirovRA <AbubakirovRA@ya.ru>
2021-12-05 19:00:53 +0500

    add task54 solution

--- workshop3/Program.cs
+++ workshop3/Program.cs
@@ -1,35 +1,21 @@
-﻿// 53. В двумерном массиве показать позиции числа, заданного пользователем или указать, что такого элемента нет
-(int, int) Main((int m, int n, int userNum)data)
+﻿// 54. В матрице чисел найти сумму элементов главной диагонали
+int Main(int m, int n)
 {
-    int[,] array = getArray(data.m, data.n);
-    for (int i = 0; i < data.m; i++)
-    {
-        for (int j = 0; j < data.n; j++)
-        {
-            if (array[i, j] == data.userNum) return (i, j);
-        }
-    }
-    return (0, 0);
-}
-
-int[,] getArray(int m, int n)
-{
-    int[,] mass = new int[m, n];
+    int sum=0; int[,] array = new int[m, n];
     for (int i = 0; i < m; i++)
     {
         for (int j = 0; j < n; j++)
         {
-            mass[i, j] = new Random().Next(10, 100);
-            Console.Write($"{mass[i,j]} ");
+            array[i,j]=new Random().Next(1, 10);
+            Console.Write($"{array[i,j]} ");
         }
-    Console.WriteLine();
+        Console.WriteLine();
     }
-    return mass;
+    for (int i = 0; i < m; i++)
+    {
+        sum +=array[i,i];
+    }
+    return sum;
 }
-
 Console.Clear();
-var data = (5, 5, 31);
-var result = Main(data);
-if (result.Item1 == 0) Console.WriteLine($"Значение {data.Item3} в массиве отсутствует");
-else Console.WriteLine($"Значение {data.Item3} является ({result.Item1}, {result.Item2})-й элементом массива ");
-
+Console.WriteLine($"Сумма элементов главной диагонали равнf: {Main(5,5)}");

66e6c71284e807b32a80afa1deb5d33226ee9d2e
AbubakirovRA <AbubakirovRA@ya.ru>
2021-12-05 18:37:11 +0500

    add task53 solution

--- workshop3/Program.cs
+++ workshop3/Program.cs
@@ -1,24 +1,35 @@
-﻿// 52. В двумерном массиве заменить элементы, у которых оба индекса чётные на их квадраты
-int[,] Main(int m, int n)
+﻿// 53. В двумерном массиве показать позиции числа, заданного пользователем или указать, что такого элемента нет
+(int, int) Main((int m, int n, int userNum)data)
 {
-    int[,] array = new int[m, n];
+    int[,] array = getArray(data.m, data.n);
+    for (int i = 0; i < data.m; i++)
+    {
+        for (int j = 0; j < data.n; j++)
+        {
+            if (array[i, j] == data.userNum) return (i, j);
+        }
+    }
+    return (0, 0);
+}
+
+int[,] getArray(int m, int n)
+{
+    int[,] mass = new int[m, n];
     for (int i = 0; i < m; i++)
     {
         for (int j = 0; j < n; j++)
         {
-            array[i, j] = new Random().Next(10, 100);
-            if (i % 2 == 0 && j % 2 == 0) array[i, j] = squareNumber(array[i, j]);
-            Console.Write($"{array[i, j]} ");
+            mass[i, j] = new Random().Next(10, 100);
+            Console.Write($"{mass[i,j]} ");
         }
-
-        Console.WriteLine();
+    Console.WriteLine();
     }
-    return array;
+    return mass;
 }
 
-int squareNumber(int arg)
-{
-    return (arg * arg);
-}
+Console.Clear();
+var data = (5, 5, 31);
+var result = Main(data);
+if (result.Item1 == 0) Console.WriteLine($"Значение {data.Item3} в массиве отсутствует");
+else Console.WriteLine($"Значение {data.Item3} является ({result.Item1}, {result.Item2})-й элементом массива ");
 
-Main(5, 5);

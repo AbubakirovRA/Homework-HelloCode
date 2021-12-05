e23c709cfe8e2d573453baa66f4ac1a80d657f55
AbubakirovRA <AbubakirovRA@ya.ru>
2021-12-05 22:58:37 +0500

    add task58 solution

--- workshop3/Program.cs
+++ workshop3/Program.cs
@@ -4,10 +4,8 @@ int[,] getArray(int row, int col)
     int[,] array = new int[row, col];
     for (int i = 0; i < row; i++)
     {
-        for (int j = 0; j < col; j++)
-        {
-            array[i, j] = new Random().Next(1, 10);
-        }
+        for (int j = 0; j < col; j++) 
+        { array[i, j] = new Random().Next(1, 10); }
     }
     return array;
 }
@@ -16,30 +14,44 @@ void PrintArray(int[,] arr)
 {
     for (int i = 0; i < arr.GetLength(0); i++)
     {
-        for (int j = 0; j < arr.GetLength(1); j++)
-        {
-            Console.Write($"{arr[i, j]}  ");
-        }
+        for (int j = 0; j < arr.GetLength(1); j++) 
+        { Console.Write("{0,4}", arr[i, j]); }
         Console.WriteLine();
     }
 }
 
-int[,] RowToColumn(int[,] array)
+int[,] RowToColumn(int[,] mass)
 {
-    for (int i = 0; i < array.GetLength(0); i++)
+    for (int i = 0; i < mass.GetLength(0); i++)
     {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            int temp = array[i, j];
-            array[i, j] = array[j, i];
-            array[j, i] = temp;
-        }
+        for (int j = 0; j < mass.GetLength(1); j++)
+        { int temp = mass[i, j]; mass[i, j] = mass[j, i]; mass[j, i] = temp; }
     }
     Console.WriteLine("Транспонированный массив:");
-    return array;
+    return mass;
 }
 Console.Clear();
 int[,] Array = getArray(5, 5);
 PrintArray(Array);
 if (Array.GetLength(0) != Array.GetLength(1)) Console.WriteLine("Невозможно транспонировать");
 else PrintArray(RowToColumn(Array));
+
+// void Myxlezh(int n)// с интернетов
+// {
+//     Random r = new Random();
+//     int[,] matrix = new int[n,n];
+//     for (int i = 0; i < n; i++)
+//     {
+//         for (int j = 0; j < n; j++) Console.Write("{0,4}", matrix[i, j] = r.Next(0, 100));
+//         Console.WriteLine();
+//     }
+//     Console.WriteLine();
+//     for (int i = 0; i < n; i++)
+//     {
+//         for (int j = 0; j < n; j++) Console.Write("{0,4}", matrix[j, i]);
+//         Console.WriteLine();
+//     }
+//     Console.ReadKey(true);
+// }
+
+// Myxlezh(5);

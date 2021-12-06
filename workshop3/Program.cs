1534f2cb868df99fa67aa51076d1d08348a6c2be
AbubakirovRA <AbubakirovRA@ya.ru>
2021-12-06 22:33:58 +0500

    add task67 solution

--- workshop3/Program.cs
+++ workshop3/Program.cs
@@ -1,10 +1,7 @@
 ﻿// 67. Показать натуральные числа от N до 1, N задано
+
 int Foo(int n)
-{
-    if (n==0) return 1;
-    Console.Write("{0,4}", n);
-    return Foo(n-1);
-}
-Foo(20);
-System.Console.WriteLine();
+{ if (n == 0) return 1; Console.Write("{0,4}", n); return Foo(n - 1); }
+
+Foo(20); System.Console.WriteLine();
 

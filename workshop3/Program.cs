52f1d2ccf20834f2a75abc0ea4c4d04f770bee0a
AbubakirovRA <AbubakirovRA@ya.ru>
2021-12-06 23:18:52 +0500

    add task69 solution

--- workshop3/Program.cs
+++ workshop3/Program.cs
@@ -1,11 +1,11 @@
-﻿// 68. Показать натуральные числа от M до N, N и M заданы
+﻿// 69. Найти сумму элементов от M до N, N и M заданы
 
 int Natural(int i, int n)
 {
-    if (n == i-1) return i;
-    else {Console.Write("{0,4}", n);
-    return  Natural(i, n-1);}
+    if (n == i-1) return n+i;
+    else n +=(n-1);
+    return  Natural(i, n-1);
 }
 
-Natural(-10, 25); Console.WriteLine();
+Natural(0, 5); Console.WriteLine();
 

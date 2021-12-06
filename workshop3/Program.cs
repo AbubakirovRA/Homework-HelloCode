a12cbbcac6f464be227ac949f53c81a00be8f8d7
AbubakirovRA <AbubakirovRA@ya.ru>
2021-12-06 16:17:22 +0500

    add task60 solution

--- workshop3/Program.cs
+++ workshop3/Program.cs
@@ -1,31 +1,105 @@
-﻿// 59. В прямоугольной матрице найти строку с наименьшей суммой элементов.
+﻿// 60. Составить частотный словарь элементов двумерного массива
+
+(int, int) FindNum(int[,] matrix, int arg)
+{
+    int count = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(0); j++)
+        {
+            if (matrix[i, j] == arg) count++;
+        }
+    }
+    return (arg, count);
+}
+
+
+
+void Foo(int[,] data)
+{
+    int max = 20; // максимальное значение в массиве data
+    int[] elements = new int[max + 1];
+    for (int i = 0; i < data.GetLength(0); i++)
+    {
+        for (int j = 0; j < data.GetLength(1); j++)
+        {
+            elements[data[i,j]]++;
+        }
+    }
+
+(int, int) FindMaxMin(int[,] matrix)
+{
+    int max = 0;
+    int min = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(0); j++)
+        {
+            if (matrix[i,j] > max) max = matrix[i,j];
+            if (matrix[i,j] < min) min = matrix[i,j];
+        }
+    }
+    return (max, min);
+}
+
+
+void Foo(int[,] data)
+{
+    var maxMin = FindMaxMin(data);
+    int max = maxMin.Item1;
+    int min = maxMin.Item2;
+    int[] elements = new int[max + Math.Abs(min) + 1];
+    for (int i = 0; i < data.GetLength(0); i++)
+    {
+        for (int j = 0; j < data.GetLength(1); j++)
+        {
+            elements[data[i,j] + Math.Abs(min)]++;
+        }
+    }
+
+    for (int i = 0; i < elements.Length; i++)
+    {
+        if(elements[i]!=0) System.Console.WriteLine($"{i - Math.Abs(min)} - встречается {elements[i]}");
+    }
+
+}
+
+
+
+
+
+
 
 Console.Clear(); int[,] Array = getArray(5, 5);
-PrintArray(Array); Console.WriteLine($"MinValue = {findMin(Array)}");
+PrintArray(Array); 
 
-int findMin(int[,] mass)
+int CheckNum(int[,] mass)
 {
     int minValue = 0;
-    for (int i = 0; i < mass.GetLength(0); i++)
+    for (int k=0; k<mass.GetLength(0)*mass.GetLength(1))
     {
-        int sum = 0;
-        for (int j = 0; j < mass.GetLength(1); j++)
+        for (int i = 0; i < mass.GetLength(0); i++)
         {
-            sum += mass[i, j];
+            int sum = 0;
+            for (int j = 0; j < mass.GetLength(1); j++)
+            {
+                sum += mass[i, j];
+            }
+            if (i == 0) minValue = sum;
+            if (minValue > sum) minValue = sum;
         }
-        if (i == 0) minValue = sum;
-        if (minValue > sum) minValue = sum;
     }
     return minValue;
 }
 
 int[,] getArray(int row, int col)
 {
+    Random r=new Random();
     int[,] array = new int[row, col];
     for (int i = 0; i < row; i++)
     {
         for (int j = 0; j < col; j++)
-        { array[i, j] = new Random().Next(1, 10); }
+        { array[i, j] = r.Next(1, 10); }
     }
     return array;
 }

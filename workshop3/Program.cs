ef0fa138cdfee7f50b53ba754b9eb9fb5ecc43b3
AbubakirovRA <AbubakirovRA@ya.ru>
2021-12-06 09:43:23 +0500

    add task57 solution

--- workshop3/Program.cs
+++ workshop3/Program.cs
@@ -24,19 +24,6 @@ int[,] Sorting(int[,] array)
 }
 Console.Clear();
 PrintArray(Sorting(FillArray(Array(6, 6))));
-// 66. Показать натуральные числа от 1 до N, N задано
-// int Natural(int i, int n)
-// {
-//     if (n == 0) return 0;
-//     else {Console.Write($"{i} ");
-//     return Natural(i += 1, n - 1);}
-// }
-
-// Natural(1, 15);
-
-
-
-
 
 int[,] Array(int m, int n) // метод создания массива
 {

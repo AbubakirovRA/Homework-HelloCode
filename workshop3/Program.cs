0eca5d7a36dfc3ea4cf85974114676549f04add1
AbubakirovRA <AbubakirovRA@ya.ru>
2021-12-06 17:54:32 +0500

    add task61 solution

--- workshop3/Program.cs
+++ workshop3/Program.cs
@@ -1,43 +1,29 @@
-﻿// 60. Составить частотный словарь элементов двумерного массива
+﻿// 61. Найти произведение двух матриц
 
-void Foo(int[,] data) // Решение от Сергея
+Console.Clear(); 
+int[,] Array1 = getArray(4, 4);
+int[,] Array2 = getArray(4, 4);
+Console.WriteLine($" Матрица №1:"); PrintArray(Array1);
+Console.WriteLine($" Матрица №2:"); PrintArray(Array2); 
+Console.WriteLine($" Произведение матриц:"); 
+if (Array1.GetLength(0)>Array2.GetLength(1) || Array2.GetLength(0)>Array2.GetLength(1)) 
+Console.WriteLine("Данные матрицы перемножить нельзя");
+else PrintArray(SumArray(Array1, Array2));
+
+
+int[,] SumArray(int[,] arr1, int[,] arr2)
 {
-    var maxMin = FindMaxMin(data);
-    int max = maxMin.Item1;
-    int min = maxMin.Item2;
-    int[] elements = new int[max + Math.Abs(min) + 1];
-    for (int i = 0; i < data.GetLength(0); i++)
+    int[,] arr3=new int[arr1.GetLength(0), arr2.GetLength(0)];
+    for (int i=0; i<arr1.GetLength(0);i++)
     {
-        for (int j = 0; j < data.GetLength(1); j++)
+        for (int j=0; j<arr2.GetLength(0); j++)
         {
-            elements[data[i,j] + Math.Abs(min)]++;
+            for (int k=0; k<arr2.GetLength(0);k++)
+                {arr3[i,j] += (arr1[i,k]*arr2[k,j]);}
         }
     }
-
-    for (int i = 0; i < elements.Length; i++)
-    {
-        if(elements[i]!=0) System.Console.WriteLine($"{i - Math.Abs(min)} - встречается {elements[i]}");
-    }
-
+    return arr3;
 }
-
-(int, int) FindMaxMin(int[,] matrix)
-{
-    int max = 0;
-    int min = 0;
-    for (int i = 0; i < matrix.GetLength(0); i++)
-    {
-        for (int j = 0; j < matrix.GetLength(0); j++)
-        {
-            if (matrix[i,j] > max) max = matrix[i,j];
-            if (matrix[i,j] < min) min = matrix[i,j];
-        }
-    }
-    return (max, min);
-}
-
-Console.Clear(); int[,] Array = getArray(5, 5);
-PrintArray(Array); Foo(Array);
 
 int[,] getArray(int row, int col)
 {
@@ -46,7 +32,7 @@ int[,] getArray(int row, int col)
     for (int i = 0; i < row; i++)
     {
         for (int j = 0; j < col; j++)
-        { array[i, j] = r.Next(-7, 10); }
+        { array[i, j] = r.Next(1, 10); }
     }
     return array;
 }
@@ -60,29 +46,3 @@ void PrintArray(int[,] arr)
     }
     Console.WriteLine();
 }
-
-// string PrintArray(int[,] arr) // Решение 1
-// {
-//     string str=string.Empty;
-//     for (int i = 0; i < arr.GetLength(0); i++)
-//     {
-//         for (int j = 0; j < arr.GetLength(1); j++)
-//         { Console.Write("{0,4}", arr[i, j]); str += $"{FindNum(arr, arr[i,j])}, "; }
-//         Console.WriteLine();
-//     }
-//     Console.WriteLine();
-//     return str;
-// }
-
-// (int, int) FindNum(int[,] matrix, int arg) // Решение 1
-// {
-//     int count = 0;
-//     for (int i = 0; i < matrix.GetLength(0); i++)
-//     {
-//         for (int j = 0; j < matrix.GetLength(0); j++)
-//         {
-//             if (matrix[i, j] == arg) count++;
-//         }
-//     }
-//     return (arg, count);
-// }

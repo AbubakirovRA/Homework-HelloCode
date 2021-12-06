a0b6fd6b072992d5b346176dbe7250a755cd7bf0
AbubakirovRA <91825137+AbubakirovRA@users.noreply.github.com>
2021-12-06 22:22:04 +0500

    Update Program.cs

--- workshop3/Program.cs
+++ workshop3/Program.cs
@@ -9,6 +9,15 @@
 
 // Natural(1, 15);
 
+// int Foo(int n) // второе решение
+// {
+//     if (n==0) return 1;
+//     Console.Write("{0,4}", Foo(n-1));
+//     return n+1;
+// }
+// Foo(10);
+// System.Console.WriteLine();
+
 int Main(string[] j, string[] s)
 {
     int count = 0;

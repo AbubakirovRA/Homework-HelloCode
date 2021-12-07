2b0c8035daf39c2848fc732432754ad773656b65
AbubakirovRA <AbubakirovRA@ya.ru>
2021-12-07 17:11:15 +0500

    add task70 solution

--- workshop3/Program.cs
+++ workshop3/Program.cs
@@ -1,11 +1,6 @@
 ﻿// 70. Найти сумму цифр числа
-
 int Sum(int n)
-{
-    if (n%10 == 0) return n;
-    else return n%10 + Sum(n/10);
-}
-
+{ if (n % 10 == 0) return n; else return n % 10 + Sum(n / 10); }
 Console.WriteLine(Sum(123456789));
 
 

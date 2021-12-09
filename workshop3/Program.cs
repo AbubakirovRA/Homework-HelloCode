0ba3b0d1dca8189ecd6d9e78d944997266277a32
AbubakirovRA <AbubakirovRA@ya.ru>
2021-12-09 18:15:59 +0500

    add task69 solution

--- workshop3/Program.cs
+++ workshop3/Program.cs
@@ -1,8 +1,12 @@
 ﻿// 69. Найти сумму элементов от M до N, N и M заданы
 
 int Sum(int n, int m)
-{ if (m == n) return m; else return m + Sum(n, m - 1); }
-Console.WriteLine(Sum(-1, 10));
+{
+    if (m < n) { int k = m; m = n; n = k;}
+    if (m == n) return m; 
+    else return m + Sum(n, m - 1);
+}
+Console.WriteLine(Sum(100,0));
 
 
 

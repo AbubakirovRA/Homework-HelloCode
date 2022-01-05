9b45f70b76535b8ff5675e6674f18d85cce8935d
AbubakirovRA <AbubakirovRA@ya.ru>
2022-01-06 00:14:21 +0500

    add folder guess_the_number

---
+++ guess_the_number/Program.cs
@@ -0,0 +1,7 @@
+﻿(int secretNumber, int countOfAttempts, string namePlayer) = Welcome();
+if (secretNumber == -99) Console.WriteLine("G A M E   O V E R!");
+else
+{
+    Console.Clear();
+    Console.WriteLine($"Ok, {namePlayer}, I guess a Number!");
+}

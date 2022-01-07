56f495c4068319153d2c07f1a99ba73b488bea02
AbubakirovRA <AbubakirovRA@ya.ru>
2022-01-07 14:26:21 +0500

    add diagramm usernswer initgame requestnumber

--- guess_the_number/Program.cs
+++ guess_the_number/Program.cs
@@ -5,3 +5,20 @@ else
     Console.Clear();
     Console.WriteLine($"Ok, {namePlayer}, I guess a Number!");
 }
+
+int RequestNumber(string words) // input of numbers with validation 
+{
+    while (true)
+    {
+        Console.Write(words);
+        if (int.TryParse(Console.ReadLine(), out int num) && num > 0) return num;
+        else Console.WriteLine("You have entered something wrong, try one more.");
+    }
+}
+
+bool userAnswer(string MessageValue) //waiting for user response 
+{
+    Console.WriteLine(MessageValue);
+    string answer = Console.ReadLine();
+    return answer.ToLower() == "y";     //if "y" is pressed, it returns true 
+}

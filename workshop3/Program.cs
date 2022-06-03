9005ba9d8d4f851f86d0c869e20a30b6694d4e35
AbubakirovRA <91825137+AbubakirovRA@users.noreply.github.com>
2022-06-04 01:47:39 +0500

    Update Program.cs

--- workshop3/Program.cs
+++ workshop3/Program.cs
@@ -1,8 +1,11 @@
 ﻿
 // 66. Показать натуральные числа от 1 до N, N задано (рекурсивной функцией)
 
-int Foo(int n) // второе решение
-{ if (n == 0) return 1; Console.Write("{0,4}", Foo(n - 1)); return n + 1; }
+int Foo(int n){  // второе решение 
+  if (n == 0) return 1; 
+  Console.Write("{0,4}", Foo(n - 1)); 
+  return n + 1; 
+}
 
 Foo(10); System.Console.WriteLine();
 
